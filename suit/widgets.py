ba67aa10935c14aa82fb1781275265996cf83fa4
Jure Zvelc <jzvelc@gmail.com>
2015-03-09 14:05:52 +0100

    fixed unicode problem

--- suit/widgets.py
+++ suit/widgets.py
@@ -239,6 +239,7 @@ def wrap_as_input_group(s, append=''):
 def adjust_widget(field):
     widget = field.field.widget
     if isinstance(widget, ForeignKeyRawIdWidget):
+        field = str(field)
         field = field.replace('RawIdAdminField', 'RawIdAdminField form-control')
         field = field.replace('<a', '<span class="input-group-btn"><a')
         field = field.replace('</a>', '<span class="glyphicon glyphicon-search"'
@@ -259,6 +260,7 @@ def adjust_widget(field):
         return mark_safe(field)
 
     elif isinstance(widget, RelatedFieldWidgetWrapper):
+        field = str(field)
         # field = field.replace('RawIdAdminField', 'RawIdAdminField form-control')
         field = field.replace('<a', '<span class="input-group-btn"><a')
         field = field.replace('</a>', '<span class="glyphicon glyphicon-plus-sign color-success"'
